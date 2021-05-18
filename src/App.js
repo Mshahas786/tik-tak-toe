8730ce016d0ff1902d5ced699aa99199916a0c49
Mshahas786 <mshahas786@gmail.com>
2021-05-18 15:55:37 +1200

    working fine need styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from "react";
 import './App.css';
-
+import Square from './Square';
+import Game from './Game';
 function App() {
   return (
     <div className="App">
-      
+      <Square/>
+      <Game/>
     </div>
   );
 }
